d42dc8f91bf8920a62f0d7d4bbe14948e6636207
Nagaev Dmitry <dinvk@mail.ru>
2026-02-04 18:53:48 +0500

    Upgrade to Next.js 16 (#7)

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,26 +1,19 @@
-import { FlatCompat } from '@eslint/eslintrc';
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'warn',
-      semi: 'error',
-    },
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
+  files: ['**/*.ts', '**/*.tsx'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    semi: 'error',
   },
-  {
-    ignores: ['.next', 'node_modules/', '**/*.js'],
-  },
-];
+}, {
+  ignores: ['.next', 'node_modules/', '**/*.js'],
+}];
 
 export default eslintConfig;
