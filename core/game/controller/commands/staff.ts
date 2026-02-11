35ca4e5f7af86408481a52abe3b4720d52522413
fng3r <dinvk@mail.ru>
2026-02-11 12:48:40 +0500

    refactor staff command

--- core/game/controller/commands/staff.ts
+++ core/game/controller/commands/staff.ts
@@ -1,20 +1,23 @@
-import {PlayerObject} from "../../model/GameObject/PlayerObject";
-import {PlayerRoles} from "../../model/PlayerRole/PlayerRoles";
+import { PlayerObject } from "../../model/GameObject/PlayerObject";
+import { PlayerRoles } from "../../model/PlayerRole/PlayerRoles";
 import { ServiceContainer } from "../../services/ServiceContainer";
 
 export function cmdStaff(byPlayer: PlayerObject): void {
     const services = ServiceContainer.getInstance();
     const playerList = services.player.getPlayerList();
     
-    const [...players] = playerList.values();
-    let staffPlayersString = players
-        .filter(player => PlayerRoles.atLeast(services.playerRole.getRole(player.id)!, PlayerRoles.S_ADM))
-        .map(player => {
-            const playerRole = services.playerRole.getRole(player.id)!.role;
-            return `${player.name}#${player.id} (${playerRole})`;
-        })
-        .join(', ');
-    staffPlayersString = `👨🏻‍💼 Staff: ${staffPlayersString || 'No staff players'}`;
+    const entries = []
+    for (const player of playerList.values()) {
+        const role = services.playerRole.getRole(player.id);
+        if (!role) {
+            continue;
+        }
+
+        if (PlayerRoles.atLeast(role, PlayerRoles.S_ADM)) {
+            entries.push(`${player.name}#${player.id} (${role.role})`);
+        }
+    }
+    const staffPlayersString = `👨🏻‍💼 Staff: ${entries.join(', ') || 'No staff players'}`;
 
     services.room.sendAnnouncement(staffPlayersString, null, 0x479947, "normal", 1);
 }
