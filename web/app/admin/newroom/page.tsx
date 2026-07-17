c7d52c140031bd70b3cafa9364c1486af8b87e1e
Nagaev Dmitry <dinvk@mail.ru>
2026-07-18 03:42:03 +0500

    Handle invalid player authentication correctly (#11)
    * handle the case when player auth is invalid
    
    * always render create room page dynamically

--- web/app/admin/newroom/page.tsx
+++ web/app/admin/newroom/page.tsx
@@ -2,6 +2,8 @@ import RoomCreate from '@/components/Admin/RoomCreate';
 
 import { getServerControlMappings } from '@/lib/server/control-plane';
 
+export const dynamic = 'force-dynamic';
+
 export default async function RoomCreatePage() {
   const mappings = await getServerControlMappings();
 
